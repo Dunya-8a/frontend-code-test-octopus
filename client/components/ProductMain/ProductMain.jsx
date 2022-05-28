bce25abcb305839e793141c4a158abc1d13dfd8a
Dünya <53619378+Dunya-8a@users.noreply.github.com>
2022-05-28 14:30:13 +0200

    Add styling to ProductMain component

--- client/components/ProductMain/ProductMain.jsx
+++ client/components/ProductMain/ProductMain.jsx
@@ -7,7 +7,7 @@ const ProductMain = ({ product: { img_url, name, power, quantity } }) => {
 		<div className={styles.ProductMain}>
 			<img src={img_url} alt={name} className={styles.ProductMain__img} />
       <h1 className={styles.ProductMain__title}>{name}</h1>
-      <p className={styles.ProductMain__details}>{power} // {quantity}</p>
+      <p className={styles.ProductMain__details}>{power} // Packet of {quantity}</p>
 		</div>
 	);
 };
