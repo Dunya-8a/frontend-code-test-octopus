1a5fb99657ee2050305d8c8fc97ac5a9f0f5e38c
Dünya <53619378+Dunya-8a@users.noreply.github.com>
2022-05-28 15:45:33 +0200

    Add styling and counter markdown to ProductAdder component

--- client/components/ProductAdder/ProductAdder.jsx
+++ client/components/ProductAdder/ProductAdder.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import styles from "./ProductAdder.module.scss";
 
-const ProductAdder = ({ price }) =>
-{
+const ProductAdder = ({ price }) => {
 	return (
 		<div className={styles.ProductAdder}>
 			{/* Convert price in pence into pounds */}
 			<h2 className={styles.ProductAdder__price}>£{price / 100}</h2>
-			<div className={styles.ProductAdder__counter}>Quantity Counter</div>
+			<div className={styles.ProductAdder__counter}>
+				<label className={styles.ProductAdder__label}>Qty</label>
+				<div className={styles.ProductAdder__mathcontainer}>
+					<button className={styles.ProductAdder__minus}>-</button>1
+					<button className={styles.ProductAdder__plus}>+</button>
+				</div>
+			</div>
 			<button className={styles.ProductAdder__adder}>Add to cart</button>
 		</div>
 	);
