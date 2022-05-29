1cbd05bf854c13b3a206960bb72cd49e128f3403
Dünya <53619378+Dunya-8a@users.noreply.github.com>
2022-05-29 12:01:14 +0200

    Add functional quantity counter using state to ProductAdder component

--- client/components/ProductAdder/ProductAdder.jsx
+++ client/components/ProductAdder/ProductAdder.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ProductAdder.module.scss";
 
 const ProductAdder = ({ price }) => {
+	const [qtyToAdd, setQtyToAdd] = useState(1);
+
+	// Click handler for minus button
+	// Do not allow zero or negative quantities
+	const subtractQty = () => {
+		if (qtyToAdd > 1) {
+			setQtyToAdd(qtyToAdd - 1);
+		}
+	};
+
+	// Click handler for plus button
+	const addQty = () => setQtyToAdd(qtyToAdd + 1);
+
 	return (
 		<div className={styles.ProductAdder}>
 			{/* Convert price in pence into pounds */}
 			<h2 className={styles.ProductAdder__price}>£{price / 100}</h2>
+			{/* Counter for the quantity to be added to cart */}
 			<div className={styles.ProductAdder__counter}>
 				<label className={styles.ProductAdder__label}>Qty</label>
 				<div className={styles.ProductAdder__mathcontainer}>
-					<button className={styles.ProductAdder__minus}>-</button>1
-					<button className={styles.ProductAdder__plus}>+</button>
+					<button onClick={subtractQty} className={styles.ProductAdder__minus}>
+						-
+					</button>
+					{qtyToAdd}
+					<button onClick={addQty} className={styles.ProductAdder__plus}>
+						+
+					</button>
 				</div>
 			</div>
 			<button className={styles.ProductAdder__adder}>Add to cart</button>
