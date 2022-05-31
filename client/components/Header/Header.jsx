78d00828163a6b670e5cda7d656555fa7d0eb9b3
Dünya <53619378+Dunya-8a@users.noreply.github.com>
2022-05-31 10:19:12 +0200

    Add visual counter for basket items to Header

--- client/components/Header/Header.jsx
+++ client/components/Header/Header.jsx
@@ -7,7 +7,6 @@ import CartContext from "../../context/CartContext";
 
 const Header = () => {
 	const currCart = useContext(CartContext);
-	// console.log(cartCount);
 	console.log(currCart);
 
 	return (
@@ -18,12 +17,14 @@ const Header = () => {
 					src="/octopus-logo.svg"
 					alt="Octopus Energy logo"
 				/>
-				<p>{currCart.cartCount}</p>
-				<img
-					src="/basket.svg"
-					alt="Your basket"
-					className={styles.Header__basket}
-				/>
+				<div>
+					<img
+						src="/basket.svg"
+						alt="Your basket"
+						className={styles.Header__basket}
+					/>
+					<div className={styles.Header__basketcount}>{currCart.cartCount}</div>
+				</div>
 			</header>
 		</>
 	);
